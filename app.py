497bc50328d40db36e45b59a8acfb3ffe35334e0
elisaquispe2011 <elisaquispe2011@gmail.com>
2025-02-12 20:59:11 +0100

    create home route

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask
 from datamanager.SQLite_data_manager import SQLiteDataManager
 
 
-new_data_manager = SQLiteDataManager("dbmovie.sqlite")
+app = Flask(__name__)
+data_manager = SQLiteDataManager("moviwebapp.sqlite")
+
 
 #new_data_manager.add_user("elisa")
 #new_data_manager.add_movie("twilight", "Catherine Hardwicke", 2008, 10, 1)
@@ -13,6 +15,14 @@ new_data_manager = SQLiteDataManager("dbmovie.sqlite")
 # movies = new_data_manager.get_user_movies(1)
 # for movie in movies:
 #     print(movie)
-
 #new_data_manager.update_movie(movie_id=1, movie_name="Twilight")
 #new_data_manager.delete_movie(2)
+
+
+@app.route("/")
+def home():
+    return "Welcome to MovieWeb App!"
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
