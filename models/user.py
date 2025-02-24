ab4bd22d73db3d313100a0faec36d85d0cc7b8f1
elisaquispe2011 <elisaquispe2011@gmail.com>
2025-02-24 12:45:47 +0100

    add user.py password

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 from models.base import Base
+from passlib.hash import bcrypt_sha256
 
 
 class User(Base):
@@ -8,6 +9,22 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(50), unique=True, nullable=False)
+    password_hash = Column(String(50), nullable=False)
+
+
+    @property
+    def password(self):
+        raise AttributeError("Password is not readable")
+    
+
+    @password.setter
+    def password(self, password):
+        # hash the password before storing
+        self.password_hash = bcrypt_sha256.hash(password)
+
+
+    def verify_password(self, password):
+        return bcrypt_sha256.verify(password, self.pasword_hash)
 
 
     def __str__(self):
