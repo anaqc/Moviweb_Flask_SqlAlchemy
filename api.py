bde3498134ea5fe7a2365478d7ca911148b272cb
elisaquispe2011 <elisaquispe2011@gmail.com>
2025-02-26 15:08:47 +0100

    add api list_user_movies

--- api.py
+++ api.py
@@ -22,3 +22,24 @@ def get_users():
     return jsonify({"users" : list_users})
 
 
+@api.route("/users/<int:user_id>/movies", methods=["GET"])
+def list_user_movies(user_id):
+    """ This function return a list of favorites movies from a user"""
+    movies = data_manager._get_user_movies(user_id)
+    list_movies = []
+    for movie in movies:
+        list_movies.append(
+            {
+                "id" : movie.id,
+                "name" : movie.name,
+                "director" : movie.director,
+                "year" : movie.year,
+                "rating": movie.rating,
+                "poster" : movie.poster,
+                "imdb_id" : movie.imdb_id,
+                "id_user" : movie.id_user
+            }
+        )
+    return jsonify({"user movies" : list_movies})
+
+
